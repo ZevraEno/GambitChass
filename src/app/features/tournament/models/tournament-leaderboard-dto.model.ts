dc20e2fc9fae4a2dac7783636914eb99494012a5
Daniel Hajdini <danielhajdini@gmail.com>
2025-03-31 14:04:42 +0200

    tournament details add matches and standings

---
+++ src/app/features/tournament/models/tournament-leaderboard-dto.model.ts
@@ -0,0 +1,8 @@
+export interface TournamentLeaderboardDtoModel{
+  pseudonym: string;
+  nbrOfMatchsPlayed: number;
+  nbrOfMatchsWon: number;
+  nbrOfMatchsLost: number;
+  nbrOfMatchsDrawn: number;
+  points: number;
+}
--- src/app/features/tournament/pages/tournament-details/tournament-details.component.ts
+++ src/app/features/tournament/pages/tournament-details/tournament-details.component.ts
@@ -1,13 +1,25 @@
 import {Component, inject} from '@angular/core';
 import {TournamentService} from '../../services/tournament.service';
 import {TournamentDetailsDtoModel} from '../../models/tournament-details-dto.model';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {Button} from 'primeng/button';
+import {Tab, TabList, TabPanel, TabPanels, Tabs} from 'primeng/tabs';
+import {NgForOf, NgIf} from '@angular/common';
+import {TournamentLeaderboardDtoModel} from '../../models/tournament-leaderboard-dto.model';
+import {TableModule} from 'primeng/table';
 
 @Component({
   selector: 'app-tournament-details',
   imports: [
-    Button
+    Button,
+    Tabs,
+    TabList,
+    Tab,
+    NgIf,
+    TabPanels,
+    TabPanel,
+    TableModule,
+    NgForOf
   ],
   templateUrl: './tournament-details.component.html',
   styleUrl: './tournament-details.component.scss'
@@ -16,13 +28,56 @@ export class TournamentDetailsComponent {
 
   private readonly _tournamentService: TournamentService = inject(TournamentService);
   private readonly _ar: ActivatedRoute = inject(ActivatedRoute);
+  private readonly _router: Router = inject(Router);
 
   tournament!: TournamentDetailsDtoModel;
+  standings!: TournamentLeaderboardDtoModel[];
 
   constructor() {
     let id: number = this._ar.snapshot.params['id'];
     this._tournamentService.getTournamentById(id).subscribe(tournament => {
       this.tournament = tournament;
+    });
+    this.getStandings(id);
+  }
+
+  //TODO: ajouter l'UserTokenDTO
+  startTournament(tournament: TournamentDetailsDtoModel) {
+    this._tournamentService.startTournament(tournament).subscribe({
+      next: datas => this.tournament = datas,
+      error: err => console.log(err)
+    });
+    this.getStandings(this.tournament.tournamentDTO.id);
+  }
+
+  //TODO: ajouter l'UserTokenDTO
+  nextRound(tournament: TournamentDetailsDtoModel) {
+    this._tournamentService.nextRound(tournament).subscribe({
+      next: datas => this.tournament = datas,
+      error: err => console.log(err)
+    });
+    this.getStandings(this.tournament.tournamentDTO.id);
+  }
+
+  //TODO: ajouter l'UserTokenDTO
+  deleteTournament(tournamentId: number) {
+    this._tournamentService.deleteTournament(tournamentId).subscribe({
+      next: () => this._router.navigate(['/tournament']),
+      error: err => console.error(err)
+    });
+  }
+
+  //TODO: ajouter l'UserTokenDTO
+  registerTournament(tournamentId: number) {
+  }
+
+  //TODO: ajouter l'UserTokenDTO
+  unregisterTournament(tournamentId: number) {
+  }
+
+  getStandings(tournamentId: number) {
+    this._tournamentService.getLeaderboard(tournamentId).subscribe(leaderboard => {
+      this.standings = leaderboard;
     })
   }
 }
